ac975832d6f0854c8692d84487f3a3e796e8aa94
Prince Singh <princesingh808087@gmail.com>
2023-01-07 14:31:30 +0530

    modified ui of filterskeleton

--- src/components/allProducts/FilterSkeleton.jsx
+++ src/components/allProducts/FilterSkeleton.jsx
@@ -5,10 +5,12 @@ const FilterSkeleton = () => {
 	return (
 		<SkeletonTheme baseColor="#bfc2c2" highlightColor="#6c7070">
 			<div className="flex flex-col">
-				<Skeleton height={10} width={30} />
-				<Skeleton height={8} width={30} count={4} />
-
-				<Skeleton circle={5} count={4} />
+				<Skeleton height={25} width={60} />
+				<Skeleton height={20} width={50} count={2} />
+				<Skeleton height={15} width={90} count={4} />
+				<Skeleton height={20} width={50} count={2} />
+				<Skeleton width={80} count={4} />
+				<Skeleton height={45} width={100} />
 			</div>
 		</SkeletonTheme>
 	);
